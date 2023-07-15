fcf37fb3a014f2a4285ef877164cf9cda7849290
Ahmed <a7medalqalawyi@bmail.com>
2023-07-15 03:08:05 +0300

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,15 @@
+
+
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String error;
+
+  AddNoteFailure(this.error);
+}
