c9712233dd9794f95a655d8a255f51c674553931
Ahmed <a7medalqalawyi@bmail.com>
2023-07-15 23:30:25 +0300

    add multi bloc providers

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,9 +1,23 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/model/note_model.dart';
 
 import 'add_note_state.dart';
 
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel model)async{
+    emit(AddNoteLoading());
+    try {
+      var notesBox= Hive.box<NoteModel>(noteBox);
+      await notesBox.add(model);
+      emit(AddNoteSuccess());
+    }  catch (error) {
+      emit(AddNoteFailure(error.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/view/screens/note_screen.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'model/note_model.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox(noteBox);
   Hive.registerAdapter(NoteModelAdapter());
@@ -17,19 +18,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      theme: ThemeData(
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: primaryColor
-        ),
-          brightness: Brightness.dark,
-        useMaterial3: true,
-
-        fontFamily: 'Poppins'
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                backgroundColor: primaryColor),
+            brightness: Brightness.dark,
+            useMaterial3: true,
+            fontFamily: 'Poppins'),
+        home: const NoteScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const NoteScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
